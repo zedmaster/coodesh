531bb5f8ad6f47badc55ea0899c9c378580ac0ab
zedmaster <zedmaster@gmail.com>
2024-04-24 05:01:45 -0300

    fix: e la vamos nós

--- user-data.sh
+++ user-data.sh
@@ -4,7 +4,7 @@ apt-get install -y nginx ruby git
 git config --global user.name "ubuntu"
 git config --global user.email "ubuntu@email.com"
 git clone https://github.com/zedmaster/coodesh.git
-cp -R /coodesh/app/* /var/www/html/
+cp -R /coodesh/* /var/www/html/
 service nginx restart
 wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
 chmod +x install
