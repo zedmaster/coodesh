0d910d344695b608fe222605d219ae8a9b043ec8
zedmaster <zedmaster@gmail.com>
2024-04-24 02:27:04 -0300

    fix: Correção do codebuild

---
+++ user-data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get update -y
+apt-get install -y nginx ruby git
+git config --global user.name "ubuntu"
+git config --global user.email "ubuntu@email.com"
+git clone https://github.com/zedmaster/coodesh.git
+cp -R coodesh/app/* /var/www/html/
+service nginx restart
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+chmod +x install
+./install auto
+service codedeploy-agent status
